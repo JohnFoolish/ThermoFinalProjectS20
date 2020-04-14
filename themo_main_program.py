63a5709809ca10b5ebbdd9c828c6dcb4a698765a
John Lewis Corker <JohnLCorker88@gmail.com>
2020-04-14 17:39:00 -0400

    First edition of Thermo code
    This is the base python code for the thermo project.

---
+++ themo_main_program.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 14 17:37:27 2020
+
+@author: John Lewis Corker and Somil Joshi
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import pandas 
+
+
+def main():
+    
